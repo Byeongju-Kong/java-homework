6de65931f96a4662aa26624ba796aaa4e271f016
Chris <bjuuuu98@gmail.com>
2023-06-09 13:30:20 +0900

    asdasd

--- src/main/java/hw/hello/grade/service/GradeService.java
+++ src/main/java/hw/hello/grade/service/GradeService.java
@@ -28,7 +28,7 @@ public class GradeService {
     }
 
     @Transactional
-    public void registerGrade(Long id, Long lectureId, Long studentId, int credit, double gradeValue) {
+    public void registerGrade(Long lectureId, Long studentId, int credit, double gradeValue) {
         Member member = memberRepository.findById(studentId)
                 .orElseThrow(NotFoundException::member);
         Lecture lecture = lectureRepository.findById(lectureId)
